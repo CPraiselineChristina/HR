d08386c62581bf8a6171dc09a28f14631758c566
Christina777 <132997261+CPraiselineChristina@users.noreply.github.com>
2023-05-09 15:06:40 +0530

    ATTRITION RATE CALCULATOR
    This is an employee attrition rate calculator, with company progress.
    
    one can check state of the company by measuring emp attrition

---
+++ attrition rate calculator (1).py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+a = int(input("enter left emp numbers"))
+r = int(input("enter working employees count "))
+
+ar = (a ) / (r) * 100
+
+print(ar)
+
+if ar>0:
+    if(ar<18.5):
+        print("your company doing good")
+    elif(BMI<=25.9):
+        print("your company needs to improve employee performance")
+    elif(BMI<29.5):
+        print("employee needs training")
+    elif(BMI<34.9):
+        print("enhance employees skills")
+    elif(BMI<39.9):
+        print("comapny is in danger")
+    elif(BMI<40):
+        print("company will get shutdown")
+
+
+# In[10]:
+
+
+(30)/(100)*100
+
+
+# In[ ]:
+
+
+
+
